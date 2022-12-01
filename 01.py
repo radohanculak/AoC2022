942ec58529092b82d450b931dbec35ac3702b313
Radovan Hanculak <rhanculak@redhat.com>
2022-12-01 08:15:09 +0100

    day1: oneliners

--- 01.py
+++ 01.py
@@ -7,6 +7,10 @@ def task1(data):
     return max(res)
 
 
+def task1_oneliner(data):
+    return max(map(lambda x: sum(map(int, x.split())), data))
+
+
 def task2(data):
     res = []
     for elem in data:
@@ -15,12 +19,19 @@ def task2(data):
     return sum(res[:3])
 
 
+def task2_oneliner(data):
+    return sum(sorted(map(lambda x: sum(map(int, x.split())), data))[-3:])
+
+
 def main():
     with open('./data/01.txt') as f:
         data = list(f.read().split("\n\n"))
     t1 = task1(data)
     t2 = task2(data)
     print(f"task1: {t1}\ntask2: {t2}")
+
+    assert task1(data) == task1_oneliner(data)
+    assert task2(data) == task2_oneliner(data)
 
 
 if __name__ == "__main__":
